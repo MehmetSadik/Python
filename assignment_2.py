8c33dfbf3f81bef14c4ebbff5bf8e8392f5d7a64
Mehmet Sadik <strustas@gmail.com>
2020-07-08 19:15:33 +0300

    ikinci odev son hali

---
+++ assignment_2.py
@@ -0,0 +1,8 @@
+age = input("Are you a cigarette addict older than 75 years old? ").title().strip() == "Yes"
+chronic = input("Do you have a severe chronic disease? ").title().strip() == "Yes"
+immune = input("Is your immune system too weak? ").title().strip() == "Yes"
+
+if age or chronic or immune == True:
+    print("You are in risky group")
+else:
+    print("You are not in risky group")
